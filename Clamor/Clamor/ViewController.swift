b97bbf8aec23f97654e36ec54ab639ac5b0f756a
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-01-02 04:39:55 -0800

    upclamsanddownclams

--- Clamor/Clamor/ViewController.swift
+++ Clamor/Clamor/ViewController.swift
@@ -1,5 +1,7 @@
 //
 //  ViewController.swift
+//
+//  ViewController.swift
 //  Clamour
 //
 //  Created by Rohan on 1/1/15.
@@ -17,24 +19,29 @@ extension UIColor {
 }
 import UIKit
 
-class ViewController: UIViewController,UITextViewDelegate {
-    
-    @IBOutlet weak var field: UITextView!
-    
-    
-    @IBOutlet weak var topTab: UIButton!
-    @IBOutlet weak var newTab: UIButton!
+class ViewController: UIViewController, UITextViewDelegate, UITableViewDelegate,  UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    var items: [String] = ["population : 0", "population: 1"];
     var magentacg:CGColor =  UIColor(rgb: 0x9999ff).CGColor
     var magenta:UIColor = UIColor(rgb: 0x9999ff)
     var white = UIColor.whiteColor()
     
     var top = false;
+    var clicked : Int = -1;
     override func viewDidLoad() {
         super.viewDidLoad()
-        field.delegate = self;
-
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.reloadData()
+        println(tableView);
         // Do any additional setup after loading the view, typically from a nib.
-
+    }
+    
+    func loadData(var content : Array<String>, var rating : Array<Int>){
+        items = [];
+        for (var i = 0; i < content.count; i++) {
+            items.append(content[i] + ": \(rating[i])");
+        }
+        tableView.reloadData();
     }
     
     func textView(textView: UITextView!, shouldChangeTextInRange: NSRange, replacementText: NSString!) {
@@ -47,27 +54,44 @@ class ViewController: UIViewController,UITextViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func newTabAction(sender: UIButton) {
-        if(top){
-            newTab.backgroundColor = magenta;
-            newTab.setTitleColor(white,forState: UIControlState())
-            topTab.backgroundColor = white
-            topTab.setTitleColor(magenta, forState: UIControlState())
-            top = false
-        }
+    
+   
+    @IBAction func topNewAction(sender: AnyObject) {
     }
-    @IBAction func topTabAction(sender: UIButton) {
-        if(!top){
-            topTab.backgroundColor = magenta;
-            topTab.setTitleColor(white,forState: UIControlState())
-            newTab.backgroundColor = white
-            newTab.setTitleColor(magenta, forState: UIControlState())
-            top = true;
+   
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        cell.addSubview(clamview);
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // when someone clicks on a row
+        clicked = indexPath.row;
+        performSegueWithIdentifier("seePost", sender: self);
+        clicked = -1;
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if(segue.identifier == "seePost"){
+            if(clicked == -1) {
+                return;
+            }
+            var pc = (segue.destinationViewController as PostViewController);
+            pc.itemstr = items[clicked];
             
         }
     }
-//    optional func tableView( tableView: UITableView,
-//        willDisplayCell cell: UITableViewCell,
-//        forRowAtIndexPath indexPath: NSIndexPath)
+    /*
+    
+    */
+    //    optional func tableView( tableView: UITableView,
+    //        willDisplayCell cell: UITableViewCell,
+    //        forRowAtIndexPath indexPath: NSIndexPath)
 }
-
