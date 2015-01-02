5d836395990ea47fe2be0b01e8eb5d089c0891c9
Neil Palleti <neilmrp@yahoo.com>
2015-01-02 01:50:44 -0800

    new view when click table item

--- Clamor/Clamor/ViewController.swift
+++ Clamor/Clamor/ViewController.swift
@@ -17,9 +17,10 @@ extension UIColor {
 }
 import UIKit
 
-class ViewController: UIViewController,UITextViewDelegate {
-    
+class ViewController: UIViewController, UITextViewDelegate, UITableViewDelegate,  UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var field: UITextView!
+    var items: [String] = ["population : 0", "population: 1"];
     
     
     @IBOutlet weak var topTab: UIButton!
@@ -29,12 +30,22 @@ class ViewController: UIViewController,UITextViewDelegate {
     var white = UIColor.whiteColor()
     
     var top = false;
+    var clicked : Int = -1;
     override func viewDidLoad() {
         super.viewDidLoad()
         field.delegate = self;
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
 
         // Do any additional setup after loading the view, typically from a nib.
 
+    }
+    
+    func loadData(var content : Array<String>, var rating : Array<Int>){
+        items = [];
+        for (var i = 0; i < content.count; i++) {
+            items.append(content[i] + ": \(rating[i])");
+        }
+        tableView.reloadData();
     }
     
     func textView(textView: UITextView!, shouldChangeTextInRange: NSRange, replacementText: NSString!) {
@@ -66,6 +77,38 @@ class ViewController: UIViewController,UITextViewDelegate {
             
         }
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // when someone clicks on a row
+        clicked = indexPath.row;
+        performSegueWithIdentifier("seePost", sender: self);
+        clicked = -1;
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if(segue.identifier == "seePost"){
+            if(clicked == -1) {
+                return;
+            }
+            var pc = (segue.destinationViewController as PostViewController);
+            pc.itemstr = items[clicked];
+ 
+        }
+    }
+    /*
+   
+    */
 //    optional func tableView( tableView: UITableView,
 //        willDisplayCell cell: UITableViewCell,
 //        forRowAtIndexPath indexPath: NSIndexPath)
